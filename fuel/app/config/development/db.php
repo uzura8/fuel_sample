9df9272364b51de48308703068346df6362f76ca
Shingo Yamada <uzuranoie@gmail.com>
2015-12-19 16:01:17 +0900

    db 設定の記述ファイルを変更

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -4,28 +4,4 @@
  */
 
 return array(
-	'default' => array(
-		'type'        => 'pdo',
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;port=3306;dbname=tmp_fuel_local',
-			'username'   => 'root',
-			'password'   => '',
-		),
-		'charset' => 'utf8',
-		'table_prefix' => '',
-		'enable_cache'   => false,
-		'readonly' => array('slave1'),
-	),
-	// SlaveDB 設定
-	'slave1' => array(
-		'type'        => 'pdo',
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;port=3307;unix_socket=/tmp/mysql.sock2;dbname=tmp_fuel_local',
-			'username'   => 'root',
-			'password'   => '',
-		),
-		'charset' => 'utf8',
-		'table_prefix' => '',
-		'enable_cache'   => false,
-	),
 );
